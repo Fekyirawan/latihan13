b1ff88515a1669304916679f3fd75e4ad4c0215b
Fekyirawan <71133960+Fekyirawan@users.noreply.github.com>
2021-01-17 18:47:30 +0700

    tugas13

--- system/app/Models/produk.php
+++ system/app/Models/produk.php
@@ -5,11 +5,14 @@ namespace App\Models;
 use App\Models\User;
 use App\Models\Traits\Attributes\ProdukAttributes;
 use App\Models\Traits\Relations\ProdukRelations;
+use Illuminate\Support\Str;
 
 class produk extends Model {
 	use ProdukAttributes,ProdukRelations;
 
 	protected $table = 'produk';
+	protected $primarykey = 'id';
+	
 
 	protected $casts = [
 		'created_at' => 'datetime',
@@ -17,4 +20,12 @@ class produk extends Model {
 		'berat' => 'decimal:2'
 
 	];
+
+	protected static function boot(){
+		parent::boot();
+
+		static::creating(function($item){
+			$item->id = (string) Str::id();
+		});
+	}
 }
